9783caba0d65d8960f24bd2f71a25c34020482d5
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-19 01:08:48 +0600

    text font color & padding added

--- WorkoutWatchDemo Watch App/ContentView.swift
+++ WorkoutWatchDemo Watch App/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .foregroundStyle(.blue)
+                .padding()
+                .border(.red, width: 3)
+                .padding()
         }
         .padding()
     }
