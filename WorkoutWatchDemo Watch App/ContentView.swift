d5ada24812f654a87beebc933c72f8614c9cb2f4
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-19 00:58:44 +0600

    Initial Commit

---
+++ WorkoutWatchDemo Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutWatchDemo Watch App
+//
+//  Created by Joynal Abedin on 19/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutWatchDemo Watch App/WorkoutWatchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutWatchDemoApp.swift
+//  WorkoutWatchDemo Watch App
+//
+//  Created by Joynal Abedin on 19/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutWatchDemo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
